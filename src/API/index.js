04c325358420613e67938156fc83f7951c0f358d
dinohan <dinohan.dev@gmail.com>
2021-05-23 01:41:19 +0900

    api 기본 틀 작성

--- src/API/index.js
+++ src/API/index.js
@@ -3,5 +3,27 @@ import axios from 'axios'
 export async function searchAddress (keyword){
     const response = await axios.get(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=${keyword}&key=AIzaSyDP5khM0bM6hxMmn1nkIO6d4I1bnHy6kdw`)
     return response;
-    
+}
+
+const EndPoint = ''
+
+export async function postSignIn(payload) {
+    const { data } = await axios.post(`${EndPoint}/sigin-in`, {
+
+    })
+    return data
+}
+
+export async function postSignUp(payload) {
+    const { data } = await axios.post(`${EndPoint}/sigin-up`, {
+
+    })
+    return data
+}
+
+export async function getRankList() {
+    const { data } = await axios.get(`${EndPoint}/rank`, {
+
+    })
+    return data
 }
